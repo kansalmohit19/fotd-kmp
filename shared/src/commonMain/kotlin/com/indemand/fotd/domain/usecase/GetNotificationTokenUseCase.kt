3385a2552163c84621dccaa1461d48dba02dfc70
Mohit Kansal <kansalmohit19@gmail.com>
2025-12-26 12:46:11 +0530

    login api modified

--- shared/src/commonMain/kotlin/com/indemand/fotd/domain/usecase/GetNotificationTokenUseCase.kt
+++ shared/src/commonMain/kotlin/com/indemand/fotd/domain/usecase/GetNotificationTokenUseCase.kt
@@ -1,17 +1,8 @@
 package com.indemand.fotd.domain.usecase
 
 import com.indemand.fotd.notification.provider.NotificationTokenProvider
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.StateFlow
 
-class GetNotificationTokenUseCase(private val notificationTokenProvider: NotificationTokenProvider) {
-    private var _token = MutableStateFlow<String?>(null)
-    val token: Flow<String> = _token.filterNotNull().map { it }
-
-    init {
-        val token = notificationTokenProvider.notificationToken.filterNotNull().toString()
-        _token.value = token
-    }
+class GetNotificationTokenUseCase(notificationTokenProvider: NotificationTokenProvider) {
+    val token: StateFlow<String?> = notificationTokenProvider.notificationToken
 }
