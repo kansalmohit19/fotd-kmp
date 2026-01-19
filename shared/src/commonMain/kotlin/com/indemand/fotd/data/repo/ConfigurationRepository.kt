6ceb681d3ceb8787acd7c39359f148cde0f7f1ab
Mohit Kansal <kansalmohit19@gmail.com>
2026-01-19 09:16:36 +0530

    For development handling local configurations

--- shared/src/commonMain/kotlin/com/indemand/fotd/data/repo/ConfigurationRepository.kt
+++ shared/src/commonMain/kotlin/com/indemand/fotd/data/repo/ConfigurationRepository.kt
@@ -1,11 +1,14 @@
 package com.indemand.fotd.data.repo
 
+import com.indemand.fotd.AssetDataSource
 import com.indemand.fotd.core.Either
 import com.indemand.fotd.core.IFailure
+import com.indemand.fotd.data.extensions.json
 import com.indemand.fotd.data.extensions.safeApiCall
 import com.indemand.fotd.data.local.Constants.ACCESS_TOKEN
 import com.indemand.fotd.data.local.LocalDataSource
 import com.indemand.fotd.data.mapper.toDomain
+import com.indemand.fotd.data.model.ConfigurationDetailsDTO
 import com.indemand.fotd.data.remote.RemoteDataSource
 import com.indemand.fotd.domain.model.ConfigurationDetails
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,17 +16,28 @@ import kotlinx.coroutines.flow.StateFlow
 
 
 class ConfigurationRepository(
-    val dataSource: RemoteDataSource, val localDataSource: LocalDataSource
+    val assetDataSource: AssetDataSource,
+    val dataSource: RemoteDataSource,
+    val localDataSource: LocalDataSource
 ) {
     private var internalConfiguration: MutableStateFlow<ConfigurationDetails?> =
         MutableStateFlow(null)
     val configuration: StateFlow<ConfigurationDetails?> get() = internalConfiguration
-
+    private val CONFIG_PATH = "config.json"
     suspend fun fetchConfiguration(): Either<ConfigurationDetails, IFailure> {
-        val result = safeApiCall(
-            //serializer = ConfigurationDetailsDTO.serializer(),
-            apiCall = { dataSource.fetchConfiguration() }, successTransform = { it.toDomain() })
-        result.also {
+        val result = if (true) {
+            getLocal(
+                input = json.decodeFromString(
+                    ConfigurationDetailsDTO.serializer(),
+                    assetDataSource.loadAssetFile(CONFIG_PATH),
+                ), successTransform = {
+                    it.toDomain()
+                })
+        } else {
+            safeApiCall(
+                //serializer = ConfigurationDetailsDTO.serializer(),
+                apiCall = { dataSource.fetchConfiguration() }, successTransform = { it.toDomain() })
+        }.also {
             internalConfiguration.value = it.successValue()
             saveToken()
         }
@@ -35,5 +49,11 @@ class ConfigurationRepository(
         if (tokenDetails?.useToken == true) {
             localDataSource.saveString(ACCESS_TOKEN, tokenDetails.accessToken)
         }
+    }
+
+    private fun <X, Y> getLocal(
+        input: X, successTransform: (X?) -> Y
+    ): Either<Y, IFailure> {
+        return Either.Success(successTransform(input))
     }
 }
